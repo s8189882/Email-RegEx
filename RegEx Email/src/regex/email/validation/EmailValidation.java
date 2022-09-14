7a5485474a08280b351fb2fb6277a54fc0f772b4
s8189882 <s8189882@gmail.com>
2022-09-14 15:27:25 +0530

    create main branch to validate email ID

---
+++ RegEx Email/src/regex/email/validation/EmailValidation.java
@@ -0,0 +1,8 @@
+package regex.email.validation;
+
+public class EmailValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Regular Expressions program to validate Email IDs!\n");
+	}
+}
